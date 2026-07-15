58f7c0085a34f4a00d276b3dedf90f89a14e1aa0
rgrrobinson-pixel <rgrrobinson@gmail.com>
2026-07-15 20:56:03 +1000

    Add sanity/schemas/index.ts - schema registry for Solo Launch template

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,19 @@
+import { siteSettings } from './siteSettings';
+import { page } from './page';
+import { hero } from './hero';
+import { about } from './about';
+import { destination } from './destination';
+import { destinationsGallery } from './destinationsGallery';
+import { faqItem } from './faqItem';
+import { itinerary } from './itinerary';
+
+export const schemaTypes = [
+  siteSettings,
+  page,
+  hero,
+  about,
+  destination,
+  destinationsGallery,
+  faqItem,
+  itinerary,
+];
